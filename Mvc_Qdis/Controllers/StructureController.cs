184021da60911fd4baad562a25bf63d1e145ae6c
chenyuwei <349371218@qq.com>
2016-08-24 18:17:03 +0800

    增加动态菜单展示功能

---
+++ Mvc_Qdis/Controllers/StructureController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Qdis.Controllers
+{
+    public class StructureController : Controller
+    {
+        //
+        // GET: /Structure/
+
+        public ActionResult organization()
+        {
+            return View();
+        }
+
+    }
+}
